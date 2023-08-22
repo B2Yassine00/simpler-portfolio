4ba5861b8769641476eac929daff120934d6a80a
B2Yassine00 <banouq90@gmail.com>
2023-08-22 14:48:26 +0100

    cast the shadows in mobile screens

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -24,7 +24,7 @@ const ProjectCard = ({
           scale: 1,
           speed: 450,
         }}
-        className='bg-gray-100 shadow-2xl hover:shadow-card p-5 rounded-2xl sm:w-[360px] w-full'
+        className='bg-gray-100 sm:shadow-2xl sm:hover:shadow-card p-5 rounded-2xl sm:w-[360px] w-full'
       >
         <div className='relative w-full h-[230px]'>
           <img
